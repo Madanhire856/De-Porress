d3712d3a91b90a12903ecc9dad8317dc035790a4
Madanhire856 <tavomadanhire@gmail.com>
2026-09-21 09:34:09 +0200

    Updates

--- SMS.Web/Areas/Config/Pages/UserGroups/Index.cshtml.cs
+++ SMS.Web/Areas/Config/Pages/UserGroups/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SMS.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -24,13 +25,26 @@ namespace SMS.Web.Areas.Config.Pages.UserGroups
         [BindProperty(SupportsGet = true)]
         public string? StatusFilter { get; set; }
 
+        // --- PAGINATION ---
+        [BindProperty(SupportsGet = true)]
+        public int PageNumber { get; set; } = 1;
+
+        [BindProperty(SupportsGet = true)]
+        public int PageSize { get; set; } = 50;
+
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+
         public void OnGet()
         {
+            if (PageNumber < 1) PageNumber = 1;
+            if (PageSize < 1) PageSize = 10;
+
             var query = _context.UserGroups
                 .Include(g => g.Creator)
                 .AsQueryable();
 
-            // Apply Search Filter
+            // Search filter
             if (!string.IsNullOrWhiteSpace(SearchTerm))
             {
                 query = query.Where(g =>
@@ -40,10 +54,21 @@ namespace SMS.Web.Areas.Config.Pages.UserGroups
 
             if (!string.IsNullOrWhiteSpace(StatusFilter) && StatusFilter != "All")
             {
-                // Example: query = query.Where(g => g.IsActive == (StatusFilter == "Active"));
+                // query = query.Where(g => g.IsActive == (StatusFilter == "Active"));
             }
 
-            UserGroups = query.OrderBy(g => g.Name).ToList();
+            // Total counts
+            TotalCount = query.Count();
+            TotalPages = (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+            if (TotalPages > 0 && PageNumber > TotalPages) PageNumber = TotalPages;
+
+            // Apply paging
+            UserGroups = query
+                .OrderBy(g => g.Name)
+                .Skip((PageNumber - 1) * PageSize)
+                .Take(PageSize)
+                .ToList();
         }
     }
 }
---
+++ SMS.Web/Areas/Config/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SMS.Web.Areas.Config.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
